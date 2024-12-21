67b745fdd04152f34c97d78d6296a08ec58311f9
ilshat kushmanbetov <ilshat26101993@list.ru>
2024-12-21 20:20:13 +0700

    test: adding file tests.py

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def get_number_from_index(i, j):
     return i * 4 + j + 1
 
 
+def get_index_from_number(num):
+    num -= 1
+    x, y = num // 4, num % 4
+    return x, y
+
+
 def get_empty_list(mas):
     empty = []
     for i in range(4):
