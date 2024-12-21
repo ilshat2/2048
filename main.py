d86a768cde7ffe986342524547535fb82a09fcf7
ilshat kushmanbetov <ilshat26101993@list.ru>
2024-12-21 20:29:40 +0700

    feat: adding random module

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def pretty_print(mas):
     print('=' * 10)
     for row in mas:
@@ -13,6 +16,14 @@ def get_index_from_number(num):
     num -= 1
     x, y = num // 4, num % 4
     return x, y
+
+
+def insert_2_or_4(mas, x, y):
+    if random.random() <= 0.75:
+        mas[x][y] = 2
+    else:
+        mas[x][y] = 4
+    return mas
 
 
 def get_empty_list(mas):
